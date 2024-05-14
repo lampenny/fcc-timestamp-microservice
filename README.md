72c0a6ebfce220497430a12f11b739d84f694703
Penny Lam <pennylam@live.com>
2024-05-14 13:04:41 +0900

    Add working solution

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Timestamp Microservice
 
-This is the boilerplate code for the Timestamp Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
+As part of FreeCodeCamp's Backend Development and APIs course, I use their boilerplate code to build out a timestamp microservice.
+
+## Getting started
+
+ - clone this repo
+ - run `yarn install`
+ - now run `./node_modules/.bin/nodemon index.js` to start the app
+
+ ##　Endpoint
+
+ It accepts a date string as a parameter and returns:
+
+ `/api/:date`
+
+```json
+{ unix: 1451001600000, utc: "Fri, 25 Dec 2015 00:00:00 GMT" }
+```
+ 
--- index.js
+++ index.js
@@ -1,32 +1,36 @@
-// index.js
-// where your node app starts
-
-// init project
-var express = require('express');
-var app = express();
+const express = require("express");
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+const cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/api/:date", (req, res) => {
+  const paramsDate = req.params.date;
+  let date;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  if (!paramsDate) {
+    date = new Date();
+  } else {
+    const checkUnix = paramsDate * 1;
+    date = isNaN(checkUnix) ? new Date(paramsDate) : new Date(checkUnix);
+  }
+
+  if (date === "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
 });
-
-
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const listener = app.listen(process.env.PORT || 3000, function () {
+  console.log("Your app is listening on port " + listener.address().port);
 });
